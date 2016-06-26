0a3fa309504431b63430cbb72f2f8c4a5d3434f7
Shren <patel.shrenil@gmail.com>
2016-06-26 04:13:23 -0400

    api pushing

--- src/watchdogs/videoCamera.java
+++ src/watchdogs/videoCamera.java
@@ -6,27 +6,38 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.rmi.server.UID;
-import java.time.Duration;
 import java.util.ArrayList;
-import java.util.UUID;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.concurrent.Future;
 
 import javax.swing.JPanel;
 
-import org.opencv.core.Core;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
 import org.opencv.core.Size;
-import org.opencv.videoio.VideoCapture;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.objdetect.Objdetect;
+import org.opencv.videoio.VideoCapture;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.async.Callback;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
 
 public class videoCamera extends JPanel {
 
@@ -38,109 +49,182 @@ public class videoCamera extends JPanel {
 	boolean inited = false;
 	int fire = 0;
 	boolean debug = false;
-	CascadeClassifier faceCascade = new CascadeClassifier("res/haarcascades_cuda/haarcascade_frontalface_alt.xml");
-	CascadeClassifier faceCascadeProfile = new CascadeClassifier("res/haarcascades_cuda/haarcascade_profileface.xml");
+	CascadeClassifier faceCascade = new CascadeClassifier(
+			"res/haarcascades_cuda/haarcascade_frontalface_alt.xml");
+	CascadeClassifier faceCascadeProfile = new CascadeClassifier(
+			"res/haarcascades_cuda/haarcascade_profileface.xml");
 	ArrayList<Face> faces = new ArrayList<Face>();
+	char[] alphabet = "_ABCDEFGHIJKLMNOPQRSTUV1234567890".toCharArray();
 
 	public videoCamera(VideoCapture cam) {
 
 		camera = cam;
 	}
-	
+
 	public class Face {
 		Rect _bounds;
-		double _destBoundsX, _destBoundsY, _destBoundsW, _destBoundsH, 
-		_drawBoundsX, _drawBoundsY, _drawBoundsW, _drawBoundsH;
+		double _destBoundsX, _destBoundsY, _destBoundsW, _destBoundsH,
+				_drawBoundsX, _drawBoundsY, _drawBoundsW, _drawBoundsH;
 		UID _id;
 		int _duration;
 		int _life;
 		boolean _sent;
 		Color color;
-		
+
+		String name, drawName, occ, drawOcc;
+
+		int rotate;
+		boolean loading;
+
 		public Face(Rect bounds) {
 			_id = new UID();
 			_bounds = bounds;
-			_destBoundsX= _destBoundsY= _destBoundsW= _destBoundsH= 
-			_drawBoundsX= _drawBoundsY= _drawBoundsW= _drawBoundsH=0;
+			_destBoundsW = _destBoundsH = _drawBoundsW = _drawBoundsH = 0;
+			_destBoundsX = _destBoundsY = _drawBoundsX = _drawBoundsY = -100;
 			_duration = 0;
 			_sent = false;
-			color = new Color((int)(Math.random() * 0x1000000));
+			color = new Color((int) (Math.random() * 0x1000000));
 			_life = 20;
+
+			name = null;
+			drawName = "BcdZaXbabYe";
+			occ = null;
+			drawOcc = "17545792740";
+
+			rotate = 0;
+			loading = false;
 		}
-		
+
 		public void keepAlive() {
 			_life = 20;
 			this._duration++;
-			
+
 			_destBoundsX = _bounds.x;
 			_destBoundsY = _bounds.y;
 			_destBoundsW = _bounds.width;
 			_destBoundsH = _bounds.height;
 		}
-		
+
 		public void update() {
 			_life--;
-			
-			if (_duration > 10){
+
+			if (_duration > 10) {
 				_drawBoundsX += -(_drawBoundsX - _destBoundsX) * 0.3;
 				_drawBoundsY += -(_drawBoundsY - _destBoundsY) * 0.3;
 				_drawBoundsW += -(_drawBoundsW - _destBoundsW) * 0.1;
 				_drawBoundsH += -(_drawBoundsH - _destBoundsH) * 0.1;
 			}
+
+			if (name == null) {
+				drawName = randofy(drawName, 6);
+			} else {
+				drawName = randoSet(drawName, name, 12);
+			}
+
+			if (occ == null) {
+				drawOcc = randofy(drawOcc, 6);
+			}
+
 		}
-		
+
 	}
-	
+
+	public String randofy(String str, int entropy) {
+		String s = str;
+		for (int i = 0; i < entropy; i++) {
+			Random random = new Random();
+			int index = random.nextInt(s.length());
+			StringBuilder myName = new StringBuilder(s);
+			myName.setCharAt(index,
+					alphabet[new Random().nextInt(alphabet.length)]);
+			s = myName.toString();
+		}
+		return s;
+	}
+
+	public String randoSet(String str, String act, int entropy) {
+
+		String s = str;
+		if (s.length() != act.length()) {
+			for (int i = 0; i < entropy; i++) {
+				Random random = new Random();
+				int index = random.nextInt(s.length());
+				StringBuilder myName = new StringBuilder(s);
+				myName.setCharAt(index,
+						alphabet[new Random().nextInt(alphabet.length)]);
+				s = myName.toString();
+			}
+
+			if (s.length() > act.length()) {
+				if (s != null && s.length() > 0) {
+					s = s.substring(0, s.length() - 1);
+				}
+				return s;
+			} else {
+				return s + alphabet[new Random().nextInt(alphabet.length)];
+			}
+
+		} else {
+			Random random = new Random();
+			int index = random.nextInt(s.length());
+			StringBuilder myName = new StringBuilder(s);
+			myName.setCharAt(index, act.charAt(index));
+			s = myName.toString();
+		}
+
+		return s;
+	}
+
 	public double compareFace(Face f1, Rect r2) {
 		Rect r1 = f1._bounds;
-		//Rect r2 = f2._bounds;
-		double x1 = r1.x + r1.width/2;
-		double y1 = r1.y + r1.height/2;
-		double x2 = r2.x + r2.width/2;
-		double y2 = r2.y + r2.height/2;
-		
-		return Math.sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
+		// Rect r2 = f2._bounds;
+		double x1 = r1.x + r1.width / 2;
+		double y1 = r1.y + r1.height / 2;
+		double x2 = r2.x + r2.width / 2;
+		double y2 = r2.y + r2.height / 2;
+
+		return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
 	}
-	
+
 	public Rect boundFace(Face f1, Rect r2) {
 		Rect r1 = f1._bounds;
-		//Rect r2 = f2._bounds;
-		double x1 = r1.x + r1.width/2;
-		double y1 = r1.y + r1.height/2;
-		
-		int nW =  (r1.width + r2.width) / 2;
-		int nH =  (r1.height + r2.height) / 2;
-		
-		double x2 = r2.x + r2.width/2;
-		double y2 = r2.y + r2.height/2;
-		
-		int xx = (int)(x2 - nW/2);
-		int yy = (int)(y2 - nH/2);
-		
+		// Rect r2 = f2._bounds;
+		double x1 = r1.x + r1.width / 2;
+		double y1 = r1.y + r1.height / 2;
+
+		int nW = (r1.width + r2.width) / 2;
+		int nH = (r1.height + r2.height) / 2;
+
+		double x2 = r2.x + r2.width / 2;
+		double y2 = r2.y + r2.height / 2;
+
+		int xx = (int) (x2 - nW / 2);
+		int yy = (int) (y2 - nH / 2);
+
 		return new Rect(xx, yy, nW, nH);
 	}
-	
+
 	public Face createFace(Rect bounds) {
-		
+
 		double min = -1;
 		Face choose = null;
 		for (Face f : faces) {
 			double c = compareFace(f, bounds);
-			if ( c < 256 && (min == -1 || c < min) ) {
-				
-				f._bounds =  bounds; //boundFace(f, bounds);//bounds;
-				
+			if (c < 256 && (min == -1 || c < min)) {
+
+				f._bounds = bounds; // boundFace(f, bounds);//bounds;
+
 				min = c;
 				choose = f;
 			}
 		}
-		
+
 		if (choose != null) {
 			choose._bounds = bounds;
 			choose.keepAlive();
 			return choose;
 		} else {
-			Face nFace =  new Face(bounds);
+			Face nFace = new Face(bounds);
 			faces.add(nFace);
 			return nFace;
 		}
@@ -210,6 +294,49 @@ public class videoCamera extends JPanel {
 		return pDistance(p.x, p.y, v.x, v.y, w.x, w.y);
 	}
 
+	public void drawDiamond(Graphics g, int _drawBoundsX, int _drawBoundsY,
+			int d, int e) {
+		if ((_drawBoundsX < d) && (_drawBoundsY > e)) {
+			int x = _drawBoundsX + ((d - _drawBoundsX) / 2);
+			int y = e + ((_drawBoundsY - e) / 2);
+			g.drawLine(_drawBoundsX, _drawBoundsY, x, y);
+			g.drawLine(x, y, _drawBoundsX, e);
+			x = _drawBoundsX - ((d - _drawBoundsX) / 2);
+			g.drawLine(_drawBoundsX, e, x, y);
+			g.drawLine(x, y, _drawBoundsX, _drawBoundsY);
+		}// end of if
+		else if ((_drawBoundsX > d) && (_drawBoundsY < e)) {
+			int x = d + ((_drawBoundsX - d) / 2);
+			int y = _drawBoundsY + ((e - _drawBoundsY) / 2);
+			g.drawLine(_drawBoundsX, _drawBoundsY, x, y);
+			g.drawLine(x, y, _drawBoundsX, e);
+			x = _drawBoundsX + ((_drawBoundsX - d) / 2);
+			y = _drawBoundsY + ((e - _drawBoundsY) / 2);
+			g.drawLine(_drawBoundsX, e, x, y);
+			g.drawLine(x, y, _drawBoundsX, _drawBoundsY);
+		}// end of else if
+		else if ((_drawBoundsX > d) && (_drawBoundsY > e)) {
+			int x = d + ((_drawBoundsX - d) / 2);
+			int y = e + ((_drawBoundsY - e) / 2);
+			g.drawLine(_drawBoundsX, _drawBoundsY, x, y);
+			g.drawLine(x, y, _drawBoundsX, e);
+			x = _drawBoundsX + ((_drawBoundsX - d) / 2);
+			y = e + ((_drawBoundsY - e) / 2);
+			g.drawLine(_drawBoundsX, e, x, y);
+			g.drawLine(x, y, _drawBoundsX, _drawBoundsY);
+		}// end of else if
+		else if ((_drawBoundsX < d) && (_drawBoundsY < e)) {
+			int x = _drawBoundsX + ((d - _drawBoundsX) / 2);
+			int y = _drawBoundsY + ((e - _drawBoundsY) / 2);
+			g.drawLine(_drawBoundsX, _drawBoundsY, x, y);
+			g.drawLine(x, y, _drawBoundsX, e);
+			x = _drawBoundsX - ((d - _drawBoundsX) / 2);
+			y = _drawBoundsY + ((e - _drawBoundsY) / 2);
+			g.drawLine(_drawBoundsX, e, x, y);
+			g.drawLine(x, y, _drawBoundsX, _drawBoundsY);
+		}// end of else if
+	}// end of method drawDiamond()
+
 	public void paintComponent(Graphics g) {
 		fire--;
 
@@ -218,35 +345,19 @@ public class videoCamera extends JPanel {
 
 		Mat mat = new Mat();
 		camera.read(mat);
-		/*Core.flip(mat, mat, 1);
-
-		Mat source = mat.clone();
-
-		Mat hist = source.clone();
-
-		Mat l_src = source.clone();
-		Mat img = mat.clone();*/
-
-		//CascadeClassifier faceCascade = new CascadeClassifier();
-		
 		/*
-		 * CascadeClassifier faceDetector = new
-		 * CascadeClassifier(videoCamera.class
-		 * .getResource("haarcascade_frontalface_alt.xml").getPath());
+		 * Core.flip(mat, mat, 1);
 		 * 
-		 * MatOfRect faceDetections = new MatOfRect();
-		 * faceDetector.detectMultiScale(img, faceDetections);
+		 * Mat source = mat.clone();
 		 * 
-		 * System.out.println(String.format("Detected %s faces",
-		 * faceDetections.toArray().length));
+		 * Mat hist = source.clone();
 		 * 
-		 * for (Rect rect : faceDetections.toArray()) { Core.rectangle(img, new
-		 * Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y +
-		 * rect.height), new Scalar(0, 255, 0)); }
+		 * Mat l_src = source.clone(); Mat img = mat.clone();
 		 */
+
 		MatOfRect facs = new MatOfRect();
 		MatOfRect facsProf = new MatOfRect();
-		
+
 		Mat grayFrame = new Mat();
 
 		// convert the frame in gray scale
@@ -267,73 +378,125 @@ public class videoCamera extends JPanel {
 		faceCascade.detectMultiScale(grayFrame, facs, 1.1, 2,
 				0 | Objdetect.CASCADE_SCALE_IMAGE, new Size(absoluteFaceSize,
 						absoluteFaceSize), new Size());
-		
+
 		faceCascadeProfile.detectMultiScale(grayFrame, facsProf, 1.1, 2,
 				0 | Objdetect.CASCADE_SCALE_IMAGE, new Size(absoluteFaceSize,
 						absoluteFaceSize), new Size());
 
 		BufferedImage image = Mat2BufferedImage(mat);
 		g.drawImage(image, 10, 10, image.getWidth(), image.getHeight(), null);
-		
+
 		Rect[] facArray = facs.toArray();
 		Rect[] facProfsArray = facsProf.toArray();
-		
+
 		Rect[] facesArray = concat(facProfsArray, facArray);
-		
-		System.out.println(facesArray.length);
-		
+
 		for (int i = 0; i < facesArray.length; i++) {
 			this.createFace(facesArray[i]);
 		}
-		
-		for (Face f : faces){
+
+		for (Face f : faces) {
 			g2.setColor(f.color);
-			g2.drawRect((int) (f._drawBoundsX),
-					(int) (f._drawBoundsY),
-					(int) (f._drawBoundsW),
-					(int) (f._drawBoundsH));
+			g2.setStroke(new BasicStroke(4));
+
+			drawDiamond(
+					g2,
+					(int) (f._drawBoundsX + f._drawBoundsW / 2),
+					(int) (f._drawBoundsY + f._drawBoundsH / 2 - f._drawBoundsH * 0.75),
+					(int) (f._drawBoundsX + f._drawBoundsW * 2),
+					(int) (f._drawBoundsY + f._drawBoundsH * 2 - f._drawBoundsH * 0.75));
+			/*
+			 * g2.drawRect((int) (f._drawBoundsX), (int) (f._drawBoundsY), (int)
+			 * (f._drawBoundsW), (int) (f._drawBoundsH));
+			 */
+
 			g2.setColor(Color.WHITE);
-			g2.drawString(new Integer(f._duration).toString(), (int)(f._drawBoundsX), (int)(f._drawBoundsY + f._drawBoundsH));
+
+			g2.drawString("Name: " + f.drawName, (int) (f._drawBoundsX),
+					(int) (f._drawBoundsY + f._drawBoundsH));
+			g2.drawString("Occupation: " + f.drawOcc, (int) (f._drawBoundsX),
+					(int) (f._drawBoundsY + f._drawBoundsH) + 16);
+
 			f.update();
-			
-			if (f._duration > 40 && !f._sent){
+
+			if (f._duration > 40 && !f._sent) {
 				f._sent = true;
 				Rect r = f._bounds;
 				r.x -= r.width * 0.25;
 				r.y -= r.height * 0.25;
 				r.width *= 1.5;
 				r.height *= 1.5;
-				
+
 				Mat m = mat.submat(r);
 				saveImage(m);
+				//f.name = "Shrenil";
+		
+						
 			}
-			
+
 		}
 		g2.setColor(Color.BLACK);
-		
+
 		ArrayList<Face> copyP = new ArrayList<Face>(faces);
-		for (Face f : copyP){
-			if (f._life <= 0){
+		for (Face f : copyP) {
+			if (f._life <= 0) {
 				faces.remove(f);
 			}
 		}
 
 	}
 
-	public void saveImage(Mat subimg){
-		Imgcodecs.imwrite("filename" + idx + ".png",subimg);
+	public void saveImage(Mat subimg) {
+		Imgcodecs.imwrite("filename" + idx + ".png", subimg);
 		idx++;
-	}
-	
-	public Rect[] concat(Rect[] a, Rect[] b) {
-		   int aLen = a.length;
-		   int bLen = b.length;
-		   Rect[] c= new Rect[aLen+bLen];
-		   System.arraycopy(a, 0, c, 0, aLen);
-		   System.arraycopy(b, 0, c, aLen, bLen);
-		   return c;
+		
+		int cols = subimg.cols();
+        int rows = subimg.rows();
+        int elemSize = (int) subimg.elemSize();
+        int type = subimg.type();
+
+		
+		try {
+			Future<HttpResponse<JsonNode>> response = Unirest.post("https://lambda-face-recognition.p.mashape.com/recognize")
+					.header("X-Mashape-Key", "QaZQD7tDdimsheu0azGvGa4N8Zkdp1hyRGFjsnxwLDrvWdgNNh")
+					.field("album", "waterloopeople")
+					.field("albumkey", "9fa61f639bd289efb4b5f19a422f15a65f2af096b6de013d0088f0b4ae86af21")
+					.field("files",  Base64.encodeBase64(FileUtils.readFileToByteArray(new File("filename" + idx + ".png"))))
+					.asJsonAsync(new Callback<JsonNode>() {
+
+					    public void failed(UnirestException e) {
+					        System.out.println("The request has failed");
+					    }
+
+					    public void completed(HttpResponse<JsonNode> response) {
+					         int code = response.getStatus();
+					         Map<String, List<String>> headers = response.getHeaders();
+					         JsonNode body = response.getBody();
+					         InputStream rawBody = response.getRawBody();
+					         System.out.println(body.toString());
+					    }
+
+					    public void cancelled() {
+					        System.out.println("The request has been cancelled");
+					    }
+					});
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-	
+		
+		
+	}
+
+	public Rect[] concat(Rect[] a, Rect[] b) {
+		int aLen = a.length;
+		int bLen = b.length;
+		Rect[] c = new Rect[aLen + bLen];
+		System.arraycopy(a, 0, c, 0, aLen);
+		System.arraycopy(b, 0, c, aLen, bLen);
+		return c;
+	}
+
 	public boolean compareMatOfPoint(MatOfPoint2f point, MatOfPoint2f approx) {
 		MatOfPoint2f c1f = new MatOfPoint2f(point.toArray());
 		MatOfPoint2f c2f = new MatOfPoint2f(approx.toArray());
